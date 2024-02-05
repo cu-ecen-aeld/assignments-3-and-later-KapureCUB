0d07788c7d8951058fc693212023011910976788
deepak <deepak@Ubuntu.myguest.virtualbox.org>
2024-02-04 23:45:07 -0700

    updated manual-linux for building kernel

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,6 +35,15 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
@@ -48,6 +57,9 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin 
+mkdir -p var/log 
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -61,20 +73,54 @@ else
 fi
 
 # TODO: Make and install busybox
+make distclean
+make defconfig
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} 
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+#${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
+#${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+PI=$(find ~/ -name ld-linux-aarch64.so.1)
+cp ${PI} ${OUTDIR}/lib 
+
+PI=$(find ~/ -name libm.so.6)
+cp ${PI} ${OUTDIR}/lib64
+
+PI=$(find ~/ -name libresolv.so.2)
+cp ${PI} ${OUTDIR}/lib64
+
+PI=$(find ~/ -name libc.so.6)
+cp ${PI} ${OUTDIR}/lib64
 
 # TODO: Make device nodes
+#sudo mknod -m 666 /dev/null c 1 3 
+#sudo mknod -m 666 /dev/console c 5 1 
 
 # TODO: Clean and build the writer utility
+FILE=$(find ~/aesd/Assignment3_part1 -name finder-app)
+cd "$FILE" 
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp ${FILE}/writer ${OUTDIR}/home
+cp ${FILE}/finder* ${OUTDIR}/home
+
+mkdir -p ${OUTDIR}/home/conf
+FILE=$(find ~/aesd/Assignment3_part1 -name assignment.txt)
+cp ${FILE} ${OUTDIR}/home/conf
+
+FILE=$(find ~/aesd/Assignment3_part1 -name username.txt)
+cp ${FILE} ${OUTDIR}/home/conf
 
 # TODO: Chown the root directory
+cd ${OUTDIR}/home && chmod 744 *
 
 # TODO: Create initramfs.cpio.gz
+cd "$OUTDIR/rootfs"
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f initramfs.cpio
