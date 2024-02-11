4472fa02958d9d8c643c00f452ff93be0f18e54c
deepak <deepak@Ubuntu.myguest.virtualbox.org>
2024-02-10 21:29:20 -0700

    added todo changes for first test

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,11 +2,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
+// convert msec to usec
+#define MILLISECOND_TO_USEC    (1000)
 
 void* threadfunc(void* thread_param)
 {
@@ -14,6 +17,35 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    int ret;
+    bool local_success = false;
+    struct thread_data* args = (struct thread_data *) thread_param;
+
+    // wait to start thread
+    usleep((args->wait_to_obtain_ms)*MILLISECOND_TO_USEC);
+
+    // call lock on mutex
+    ret = pthread_mutex_lock(args->mutex);
+    if(ret != 0) {
+        ERROR_LOG("Error in acquiring lock in TID %d. %s", pthread_self(), strerror(errno));
+    }
+    
+    // wait for relaese
+    usleep((args->wait_to_release_ms)*MILLISECOND_TO_USEC);
+    
+    // call unlock on mutex
+    ret = pthread_mutex_unlock(args->mutex);
+    if(ret != 0) {
+        ERROR_LOG("Error in releasing lock in TID %d. %s", pthread_self(), strerror(errno));
+    } else {
+        local_success = true;
+    }
+    
+    // set success in return struct parameters
+    if(local_success) {
+        args->thread_complete_success = true;
+    }
+
     return thread_param;
 }
 
@@ -28,6 +60,28 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    // local variables
+    bool ret = false;
+    int ret_thread;
+
+    // dynamically allocated memory for thread data
+    thread_data *thread_param             = malloc(sizeof(thread_data));
+    
+    // filling in the thread data parameters
+    thread_param->wait_to_obtain_ms       = wait_to_obtain_ms;
+    thread_param->wait_to_release_ms      = wait_to_release_ms;
+    thread_param->mutex                   = mutex;
+    thread_param->thread_complete_success = false;
+
+    // create thread
+    ret_thread = pthread_create(thread, NULL, threadfunc, thread_param);
+    if(ret_thread == 0) {
+        DEBUG_LOG("Thread with TID %d started succesfully.", *thread);
+        ret = true;
+    } else {
+        ERROR_LOG("Error creating thread with TID %d. %s" *thread, strerror(errno));
+    }
+
+    return ret;
 }
 
