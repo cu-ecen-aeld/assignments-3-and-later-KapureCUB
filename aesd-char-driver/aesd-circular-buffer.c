d8693169e4f88d3cf6a67853b1fc399e47a5d445
deepak <deepak@Ubuntu.myguest.virtualbox.org>
2024-03-06 21:07:08 -0700

    working circular buffer implementation

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -15,7 +15,7 @@
 #endif
 
 #include "aesd-circular-buffer.h"
-
+#include <stdio.h>    // added for printf
 /**
  * @param buffer the buffer to search for corresponding offset.  Any necessary locking must be performed by caller.
  * @param char_offset the position to search for in the buffer list, describing the zero referenced
@@ -29,10 +29,50 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
-    return NULL;
+    bool failed_to_find = false;
+    struct aesd_buffer_entry *ret = NULL; 
+    // check for args validity
+    if(buffer != NULL) {
+        if(entry_offset_byte_rtn != NULL) {
+            size_t local_char_offset              = char_offset+1;
+            uint8_t local_out_offs                = buffer->out_offs;
+            uint8_t local_in_offs                 = buffer->in_offs;
+            struct aesd_buffer_entry *local_entry = &((buffer->entry)[local_out_offs]); 
+            // iterate till desired entry reached
+            while(local_char_offset > (local_entry->size)) {
+                // check if out_offs is within in_offs
+                if((buffer->full) && (buffer->in_offs == 0) && (buffer->out_offs == 0)) {
+                    if(local_out_offs == (AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED - 1)) {
+                        failed_to_find = true;
+                        break;
+                    }
+                } else {
+                    if(local_out_offs == (local_in_offs -1)) {
+                        failed_to_find = true;
+                        break;
+                    }
+                }
+                // shift to next entry and retry
+                local_char_offset -= (local_entry->size);
+                local_out_offs = (local_out_offs == (AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED - 1)) ? \
+                                  0:local_out_offs+1;
+                local_entry = &((buffer->entry)[local_out_offs]);
+            }
+            // set return parameters
+            if(!failed_to_find) {
+                *entry_offset_byte_rtn = (local_char_offset-1);
+                ret = local_entry;
+            }
+        } else {
+            // invalid add_entry parameter. Report error
+            printf("Invalid buffer pointer passed to fops\n");
+        }    
+    } else {
+        // invalid buffer parameter. Report error
+        printf("Invalid entry_offset_byte_rtn pointer passed to fops\n");
+    }
+
+    return ret;
 }
 
 /**
@@ -44,9 +84,38 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description
-    */
+    // check for args validity
+    if(buffer != NULL) {
+        if(add_entry != NULL) {
+
+            uint8_t local_in_offs   = buffer->in_offs;
+            uint8_t local_out_offs  = buffer->out_offs;
+
+            // adding element to buffer
+            buffer->entry[local_in_offs] = *add_entry;
+            // setting input and output buffer offsets
+            if(buffer->full) {    
+                local_out_offs = (local_out_offs == (AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED - 1)) ? \
+                                  0:local_out_offs+1;
+            }
+            local_in_offs = (local_in_offs == (AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED - 1)) ? \
+                             0:local_in_offs+1;
+
+            buffer->in_offs  = local_in_offs;
+            buffer->out_offs = local_out_offs;
+            if(local_in_offs == local_out_offs) {
+                // buffer full. Set flag
+                buffer->full = true;
+            }
+        } else {
+            // invalid add_entry parameter. Report error
+            printf("Invalid buffer pointer passed to add_entry\n");
+        }    
+    } else {
+        // invalid buffer parameter. Report error
+        printf("Invalid entry pointer passed to add_entry\n");
+    }
+    
 }
 
 /**
